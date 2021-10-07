2b53eb9c2e884e2c7aa81ad45013d0b2733e22ea
zeckem19 <50004209+zeckem19@users.noreply.github.com>
2021-10-08 01:34:19 +0800

    add call for check kiosk empty

---
+++ creation/create_kiosk.py
@@ -0,0 +1,35 @@
+import os
+import certifi
+from datetime import datetime
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MURL = os.getenv('MURL')
+DBNAME = os.getenv('DBNAME')
+
+client = MongoClient(MURL, tlsCAFile=certifi.where())
+db = client[DBNAME]
+
+kiosk_collection = db['kiosk']
+
+kiosk_collection.delete_many({})
+
+
+patient_collection = db['patient']
+
+patient_collection.delete_many({})
+
+kiosk = {
+    'id':'K00000',
+    'state':'default',
+    'creation_time':datetime.now(),
+    'name':'TEST KIOSK2',
+    'address':'MOCK ADDRESS',
+    'image':'',
+    'location':'',
+    'queue':[]
+}
+
+kiosk_collection.insert_one(kiosk)
